242aee429c09b89fbb19299a21589305720a1db3
sorinpark <sorinpark0328@gmail.com>
2025-11-06 12:48:26 +0900

    feat: 카메라/프로필/설정/친구 페이지 및 ui구현

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    allowedHosts: [
+      // ngrok의 새 .dev 도메인을 허용
+      '.ngrok-free.dev',
+      
+      // (혹시 모르니 예전 .app 도메인도 추가)
+      '.ngrok-free.app' 
+    ]
+  }
+  
 })
