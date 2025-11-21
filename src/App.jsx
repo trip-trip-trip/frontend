b086baad98c2bb64b8569ed719721d453ae29f01
geummi0520 <rmaal520@gmail.com>
2025-11-21 16:32:36 +0900

    머징충돌해결

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,6 @@ import SharePhoto from './pages/Album/SharePhoto';
 import SelectPlace from './pages/Album/CreateTrip/SelectPlace';
 import PostEdit from './pages/Home/post/PostEdit';
 
-
 const App = () => {
   // 스크린 사이즈 세팅
   function setScreenSize() {
