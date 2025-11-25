c762507ca7d746e783a3e6d1d6f6273075a7c995
alua <alua.dzhumagul2005@example.com>
2025-11-25 10:43:35 +0900

    마이페이지 프사 수정 #2

--- src/pages/MyPage/ProfilePage.jsx
+++ src/pages/MyPage/ProfilePage.jsx
@@ -110,7 +110,7 @@ return (
            <img 
              src={safeUser.avatarUrl} 
              alt="프로필" 
-             className="profile-img" 
+             className="profile-img-my" 
              onError={(e) => {e.target.src = defaultProfile;}}
            />
          </div>
