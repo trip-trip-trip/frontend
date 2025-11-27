229ff1bffd729b9c3d0c73d0c2cda9e3d6f9e34e
sorinpark <sorinpark0328@gmail.com>
2025-11-28 01:15:04 +0900

    sw.js수정

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-import { precacheAndRoute  } from "workbox-precaching";
-precacheAndRoute(self.__WB_MANIFEST || []);
+//import { precacheAndRoute  } from "workbox-precaching";
+//precacheAndRoute(self.__WB_MANIFEST || []);
 
 self.addEventListener("install", () => {
   console.log(" Service Worker installed");
